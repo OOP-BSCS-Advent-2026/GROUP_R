f03679e0d73e9972c70a98a71d9e2b7cca309b6b
othienodavid <320272062+othienodavid@users.noreply.github.com>
2026-08-28 15:50:38 +0300

    this is the starting point of the assignment

---
+++ Desktop/git_trial_oop/DairyFreshShop.java
@@ -0,0 +1,19 @@
+ public class DairyFreshShop {
+
+    public static void main(String[] args) {
+
+        // Store item names and prices in arrays
+        String[] items = {"Milk (litre)", "Yogurt", "Cheese", "Eggs (tray)"};
+        double[] prices = {2500.00, 3000.00, 8000.00, 12000.00};
+
+        // Customer quantities
+        int[] quantities = {5, 2, 2, 4};
+
+    }   
+
+
+    
+    
+}
+    
+
