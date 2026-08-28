ee22c3261aa4767ebed2d43669e60595215e22fb
Jaethro loono <214395171+jeroae@users.noreply.github.com>
2026-08-28 17:48:39 +0300

    this is method one
    Added methods to display price list and calculate subtotals.

--- Desktop/git_trial_oop/DairyFreshShop.java
+++ Desktop/git_trial_oop/DairyFreshShop.java
@@ -8,6 +8,38 @@
 
         // Customer quantities
         int[] quantities = {5, 2, 2, 4};
+      // Display the shop price list
+        displayPriceList(items, prices);
+
+        // Calculate the discounted subtotal for each item
+        double[] subtotals = new double[items.length];
+
+        for (int i = 0; i < items.length; i++) {
+            subtotals[i] = calculateSubtotal(
+                    items[i],
+                    prices[i],
+                    quantities[i]
+            );
+        }
+
+        // Print the receipt and grand total
+        printReceipt(items, prices, quantities, subtotals);
+    }
+
+
+    // Method 1: Display the price list using a loop
+    public static void displayPriceList(String[] items, double[] prices) {
+
+        System.out.println("==== DAIRYFRESH SHOP ====");
+        System.out.println("PRICE LIST");
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.printf("%d. %-15s UGX %.2f%n",
+                    i + 1, items[i], prices[i]);
+        }
+
+        System.out.println();
+    }
 
     }   
 
