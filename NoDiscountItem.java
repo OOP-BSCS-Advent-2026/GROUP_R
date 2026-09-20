cd7b5f4cb709aa31d8eb6a4b6cabc16ddb1ec2f1
reymondkasozi-create <321817541+reymondkasozi-create@users.noreply.github.com>
2026-09-20 22:34:31 +0300

    Create NoDiscountItem class extending Item

---
+++ NoDiscountItem.java
@@ -0,0 +1,8 @@
+public class NoDiscountItem extends Item { //inheritance.
+
+    public NoDiscountItem(String name, double price) {
+        super(name, price);//calls the constructor of the parent class.
+    }
+
+    // No override needed.
+}
