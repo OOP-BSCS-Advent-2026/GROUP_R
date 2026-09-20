c38961edc412512c6f8a1c1c77f8c0a7b1f17d12
Jethro loono <214395171+jeroae@users.noreply.github.com>
2026-09-20 21:53:10 +0300

    All items in the shop

--- Main.java
+++ Main.java
@@ -64,3 +64,42 @@ public class Main { // main class.
     }
 }
 
+public class Item { // parent class for all items in the shop.
+
+    private String name; // variable to hold the name of the item.
+    private double price; // variable to hold the price of the item.
+
+    public Item(String name, double price) {
+        if (price <= 0) { // if the price is less than or equal to zero, throw an exception
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() { // an access modifier for accessing the name of the item.
+        return name;
+    }
+
+    public double getPrice() { // an access modifier for accessing the price of the item.
+        return price;
+    }
+
+    public double calculateTotal(int quantity) { // method to calculate the total cost of the item based on the quantity.
+
+        if (quantity < 0) { // if the quantity is less than zero, throw an exception.
+            throw new IllegalArgumentException("Quantity cannot be negative");
+        }
+
+          // return the total cost of the item.
+        return price * quantity; 
+    }
+          // method to get the discount description for the item. 
+    public String getDiscountDescription(int quantity) { 
+     return "No discount";
+    }
+} 
+    
+
+
